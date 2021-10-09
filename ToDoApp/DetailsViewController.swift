98c52d3982c6265cf2843cca89541be79a7dcedb
Ulas Goktas <u.ulas.goktas@gmail.com>
2021-10-09 21:58:47 +0300

    Add title, detail UITextFields and edit bar button item.

--- ToDoApp/DetailsViewController.swift
+++ ToDoApp/DetailsViewController.swift
@@ -10,12 +10,38 @@ import UIKit
 class DetailsViewController: UIViewController {
 
     var selectedTodo: CoreDataManager.TodoItem?
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var detailTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        titleLabel.text = selectedTodo?.title
+        
+        titleTextField.text = selectedTodo?.title
+        detailTextField.text = selectedTodo?.detail
+        titleTextField.placeholder = "Todo title"
+        detailTextField.placeholder = "Todo description"
+        
+        titleTextField.isUserInteractionEnabled = false
+        detailTextField.isUserInteractionEnabled = false
+        
+        //create edit button
+        self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
+    
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        
+        if self.isEditing {
+            self.editButtonItem.title = "Done"
+            toggleUserInteraction()
+        } else {
+            self.editButtonItem.title = "Edit"
+            toggleUserInteraction()
+        }
+    }
+    
+    func toggleUserInteraction() -> Void {
+        titleTextField.isUserInteractionEnabled.toggle()
+        detailTextField.isUserInteractionEnabled.toggle()
+    }
 }
